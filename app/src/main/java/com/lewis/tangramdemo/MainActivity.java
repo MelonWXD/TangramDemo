d05cd1e0bfd15183549ab987ac1fcb45dc57cfbe
Lewis <a549295838@163.com>
2019-01-16 17:10:33 +0800

    干掉调试代码

--- app/src/main/java/com/lewis/tangramdemo/MainActivity.java
+++ app/src/main/java/com/lewis/tangramdemo/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
-        startActivity(L6Activity.class);//调试用
     }
 
     @Override
